39ba1bf93a1e2e19508e24ab6e7605441b2aef1c
Mateusz Zając <mateusz.zajac@codete.co>
2014-11-15 13:32:04 +0100

    Ranging beacons

--- Find My Luggage/Find My Luggage/ViewController.swift
+++ Find My Luggage/Find My Luggage/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+let PROXIMITY_ID = "f7826da6-4fa2-4e98-8024-bc5b71e0893e"
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    let locationManager = CLLocationManager()
+    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: PROXIMITY_ID), identifier: "Beacon")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startRangingBeaconsInRegion(region)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: CLLocationManagerDelegate Methods
+    
+    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
+        println(beacons)
+    }
 
 }
 
