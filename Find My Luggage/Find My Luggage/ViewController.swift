7653e64d58bad44f7ae072bb59e50cc28217d6f7
Mateusz Zając <mateusz.zajac@codete.co>
2014-11-15 11:56:13 +0100

    Initialized Xcode project

---
+++ Find My Luggage/Find My Luggage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Find My Luggage
+//
+//  Created by Mateusz Zając on 15.11.2014.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
