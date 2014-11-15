f5a8d79272559cb54ab46f837049be5c38a13bde
Tomek Cejner <tomek.cejner@gmail.com>
2014-11-15 15:54:17 +0100

    User interface mock

--- Find My Luggage/Find My Luggage/ViewController.swift
+++ Find My Luggage/Find My Luggage/ViewController.swift
@@ -10,15 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var immediateRangeView: UIView!
+    @IBOutlet var nearRangeView: UIView!
+    @IBOutlet var farRangeView: UIView!
+    
     var locator:BeaconLocator?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        println("Immediate: \(immediateRangeView.frame)")
+        
+        
+        
+        let l = makeLabel("✿ Plecak 3,56m", x: 10, y: 10)
+        immediateRangeView.addSubview(l)
+        
+        
+        
         locator = BeaconLocator()
     }
 
+    func makeLabel(text:String, x:CGFloat, y:CGFloat) -> UILabel {
+        let l = UILabel(frame: CGRectMake(10, 10, 100, 30))
+        l.font = UIFont.boldSystemFontOfSize(17)
+        l.text = text
+        l.textColor = UIColor.whiteColor()
+        l.sizeToFit()
+        return l
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
