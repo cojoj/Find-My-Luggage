eaeebf4daeb044b3fa1870e32654a9d23e9bc4d4
Tomek Cejner <tomek.cejner@gmail.com>
2014-11-16 12:41:28 +0100

    Stub view controllers for managing beacons

---
+++ Find My Luggage/Find My Luggage/AddBeaconVC.swift
@@ -0,0 +1,38 @@
+//
+//  AddBeaconVC.swift
+//  Find My Luggage
+//
+//  Created by Tomek Cejner on 16/11/14.
+//
+//
+
+import UIKit
+
+class AddBeaconVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func doneTapped(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
